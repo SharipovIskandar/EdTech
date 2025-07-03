a9360c9b6dac8f7c6b258bdd1cc1102ad8a84497
Iskandar Sharipov <koklok444@gmail.com>
2025-07-03 12:17:38 +0500

    update

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Api;
 use App\Http\Requests\Api\RegisterRequest;
 use App\Http\Requests\Api\AuthRequest;
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -13,7 +15,7 @@ class AuthController extends Controller
     {
         $user = User::where('email', $request->email)->first();
         if(!$user || !Hash::check($request->password, $user->password)){
-            return 
+            return error_response('Login or password is incorrect');
         }
     }
     public function register(RegisterRequest $request) {}
